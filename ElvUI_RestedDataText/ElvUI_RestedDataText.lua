75087f4092e39593b8ba63331b9fa933b9df1bfa
Zack Hill <zackary.t.hill@gmail.com>
2023-03-29 11:00:27 -0400

    big refactor and bug fix

--- ElvUI_RestedDataText/ElvUI_RestedDataText.lua
+++ ElvUI_RestedDataText/ElvUI_RestedDataText.lua
@@ -1,12 +1,10 @@
 local E, L, V, P, G = unpack(ElvUI)
 local DT = E:GetModule('DataTexts')
-local B = E:GetModule('Bags')
 -- GLOBALS: ElvDB
 
 local wipe, pairs, ipairs, sort = wipe, pairs, ipairs, sort
 local format, strjoin, tinsert = format, strjoin, tinsert
 
-local _G = _G
 local EasyMenu = EasyMenu
 local IsLoggedIn = IsLoggedIn
 local IsShiftKeyDown = IsShiftKeyDown
@@ -38,9 +36,6 @@ end
 
 local function deleteCharacter(_, realm, name)
 	ElvDB.rested[realm][name] = nil
-	ElvDB.class[realm][name] = nil
-	ElvDB.faction[realm][name] = nil
-	ElvDB.race[realm][name] = nil
 
 	DT:ForceUpdate_DataText('Rested')
 end
@@ -51,84 +46,70 @@ local function round(num, decimals)
     return Round(num * mult) / mult
 end
 
-local function updateRested(self, updateAll)
-	if updateAll then
-		wipe(myRested)
-		wipe(menuList)
+local function updateRested(self)
+	wipe(myRested)
+	wipe(menuList)
 
-		tinsert(menuList, { text = '', isTitle = true, notCheckable = true })
-		tinsert(menuList, { text = 'Delete Character', isTitle = true, notCheckable = true })
+	tinsert(menuList, { text = '', isTitle = true, notCheckable = true })
+	tinsert(menuList, { text = 'Delete Character', isTitle = true, notCheckable = true })
 
-		local realmN = 1
-		for realm in pairs(ElvDB.serverID[E.serverID]) do
-			tinsert(menuList, realmN, { 
-				text = 'Delete All - '..realm, 
-				notCheckable = true, 
-				func = function() wipe(ElvDB.rested[realm]) DT:ForceUpdate_DataText('Rested') end 
-			})
-			realmN = realmN + 1
-			for name in pairs(ElvDB.rested[realm]) do
-				local faction = ElvDB.faction[realm][name]
-				local restedData = ElvDB.rested[realm][name]
-				local race = ElvDB.race[realm][name]
+	local realmN = 1
+	for realm in pairs(ElvDB.serverID[E.serverID]) do
+		tinsert(menuList, realmN, { 
+			text = 'Delete All - '..realm, 
+			notCheckable = true, 
+			func = function() wipe(ElvDB.rested[realm]) DT:ForceUpdate_DataText('Rested') end 
+		})
+		realmN = realmN + 1
+		for name in pairs(ElvDB.rested[realm]) do
+			local restedData = ElvDB.rested[realm][name]
 
-				if restedData then
-					local color = E:ClassColor(ElvDB.class[realm][name]) or PRIEST_COLOR
+			if restedData then
+				local color = E:ClassColor(string.gsub(string.upper(restedData.class), "%s+", "")) or PRIEST_COLOR
 
-					local restedPercentStart = (restedData.rested / restedData.xpMax) * 100
-					local now = time()
-					local secondsSinceUpdate = now - restedData.updated
+				local restedPercentStart = (restedData.rested / restedData.xpMax) * 100
+				local now = time()
+				local secondsSinceUpdate = now - restedData.updated
 
-					-- Base 5% every 8 hours
-					local restedRatePerHour = 5/8
-					local nonRestedRatePerHour = 1.25/8
-					local effectiveRestingRatePerHour = restedData.resting and restedRatePerHour or nonRestedRatePerHour
-					local restedCap = 150
+				-- Base 5% every 8 hours
+				local restedRatePerHour = 5/8
+				local effectiveRestingRatePerHour = restedData.resting and restedRatePerHour or restedRatePerHour/4
+				local restedCap = 150
 
-					if race == 'Pandaren' then
-						effectiveRestingRatePerHour = effectiveRestingRatePerHour * 2
-						restedCap = restedCap * 2
-					end
-
-					local restedPercentAccrued = effectiveRestingRatePerHour * (secondsSinceUpdate / 3600)
-					local restedPercentProjected = min(restedCap, restedPercentStart + restedPercentAccrued)
-					local restedPercentProjected = round(restedPercentProjected, 1)
-
-					-- TODO: Make level text gray if capped
-					local levelText = format('Level %0d', restedData.level)
-					local restedValueText = restedData.level == 70 and '' or format(' - %0d%% Rested', restedPercentProjected)
-					local restingStatusText = (restedData.resting or restedPercentProjected == restedCap) and '' or '|cFFFF0000[Not Resting]|r '
-					local text = restingStatusText .. levelText ..  restedValueText
-
-					tinsert(myRested, {
-							name = name,
-							realm = realm,
-							faction = faction or '',
-							level = restedData.level,
-							xp = restedData.xp,
-							restedPercent = restedPercentProjected,
-							text = text,
-							r = color.r, 
-							g = color.g, 
-							b = color.b,
-					})
-
-					tinsert(menuList, {
-						text = format('%s - %s', name, realm),
-						notCheckable = true,
-						func = function() deleteCharacter(self, realm, name) end
-					})
+				if restedData.race == 'Pandaren' then
+					effectiveRestingRatePerHour = effectiveRestingRatePerHour * 2
+					restedCap = restedCap * 2
 				end
-			end
-		end
-	else
-		for _, info in ipairs(myRested) do
-			if info.name == E.myname and info.realm == E.myrealm then
-				-- TODO: Consider adding this path back to update only current line?
-				-- info.amount = ElvDB.rested[E.myrealm][E.myname]
-				-- info.amountText = E:FormatMoney(ElvDB.rested[E.myrealm][E.myname], style, textOnly)
 
-				break
+				local secondsPerHour = 3600
+				local restedPercentAccrued = effectiveRestingRatePerHour * (secondsSinceUpdate / secondsPerHour)
+				local restedPercentProjected = min(restedCap, restedPercentStart + restedPercentAccrued)
+				local restedPercentProjected = round(restedPercentProjected, 1)
+
+				-- TODO: Make level text gray if capped
+				local levelText = format('Level %0d', restedData.level)
+				local restedValueText = restedData.level == 70 and '' or format(' - %0d%% Rested', restedPercentProjected)
+				local restingStatusText = (restedData.resting or restedPercentProjected == restedCap) and '' or '|cFFFF0000[Not Resting]|r '
+				local text = restingStatusText .. levelText ..  restedValueText
+
+				tinsert(myRested, {
+						name = name,
+						realm = realm,
+						faction = restedData.faction,
+						level = restedData.level,
+						xp = restedData.xp,
+						restedPercent = restedPercentProjected,
+						text = text,
+						r = color.r, 
+						g = color.g, 
+						b = color.b,
+				})
+
+				tinsert(menuList, {
+					text = format('%s - %s', name, realm),
+					notCheckable = true,
+					func = function() deleteCharacter(self, realm, name) end
+				})
 			end
 		end
 	end
@@ -141,15 +122,6 @@ local function OnEvent(self, event)
 		db = E.global.datatexts.settings[self.name]
 	end
 
-	-- TODO: Clean up this init code
-	if not ElvDB.race then
-		ElvDB.race = {}
-	end
-
-	if not ElvDB.race[E.myrealm] then
-		ElvDB.race[E.myrealm] = {}
-	end
-
 	if not ElvDB.rested then
 		ElvDB.rested = {}
 	end
@@ -157,25 +129,20 @@ local function OnEvent(self, event)
 	if not ElvDB.rested[E.myrealm] then
 		ElvDB.rested[E.myrealm] = {}
 	end
-
-	ElvDB.race[E.myrealm][E.myname] = UnitRace('player')
 	
-	local level = UnitLevel('player')
-	local xp = UnitXP('player')
-	local xpMax = UnitXPMax('player')
-	local rested = GetXPExhaustion() or 0
-	local resting = IsResting()
-
 	ElvDB.rested[E.myrealm][E.myname] = {
+		faction = UnitFactionGroup('player'),
+		race = UnitRace('player'),
+		class = UnitClass('player'),
 		level = UnitLevel('player'),
 		xp = UnitXP('player'),
-		xpMax = xpMax,
+		xpMax = UnitXPMax('player'),
 		rested = GetXPExhaustion() or 0,
 		resting = IsResting(),
 		updated = time(),
 	}
 
-	updateRested(self, event == 'ELVUI_FORCE_UPDATE')
+	updateRested(self)
 
 	-- TODO: Change this text?
 	self.text:SetText('Rested')
@@ -212,5 +179,10 @@ local function OnEnter()
 	DT.tooltip:Show()
 end
 
--- TODO: Register proper hooks, make sure it updates on logout
-DT:RegisterDatatext('Rested', nil, {}, OnEvent, nil, Click, OnEnter, nil, L["Rested"])
+local events = {
+	'ELVUI_FORCE_UPDATE',
+	'PLAYER_UPDATE_RESTING',
+	'PLAYER_XP_UPDATE',
+}
+
+DT:RegisterDatatext('Rested', nil, events, OnEvent, nil, Click, OnEnter, nil, L["Rested"])
